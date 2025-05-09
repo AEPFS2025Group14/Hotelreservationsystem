30c55f9082f6c72b68d933f38c598f157fb32aff
KatharinaHagen <1katharinahagen@gmail.com>
2025-05-10 00:42:08 +0200

    Model:Facilities

--- model/RoomFacility.py
+++ model/RoomFacility.py
@@ -1,3 +1,6 @@
+from model import Room
+
+
 class RoomFacility:
     def __init__(self, room_id: int, facility_id: int):
         if not room_id:
@@ -12,6 +15,28 @@ class RoomFacility:
 
         self.__room_id : int  = room_id
         self.__facility_id : int = facility_id
+        if Room is not None:
+            Room.add_facility(self)
 
     def __repr__(self):
         return (f"RoomFacility(room_id={self.__room_id}, facility_id={self.facility_id})")
+
+
+    @property
+    def Room(self) -> Room:
+        return self.__room_id
+
+    @Room.setter
+    def Room(self, rooom : Room) -> None:
+        if self.__room is not Room:
+            if self.__room is not None:
+                self.__room.remove_RoomFacility(self)
+            self.__room = Room
+
+            if Room is not None and self not in Room.RoomFacility:
+                Room.add_facility(self)
+
+
+
+
+    raise ValueError("Room must be an instance of Room")
